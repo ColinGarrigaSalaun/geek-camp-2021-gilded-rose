7d6f7de7b8dc9ac10d2a6d0607a8889282ad09f7
Colin Garriga-Salaün <colin.garriga@gmail.com>
2021-06-16 21:29:59 +0200

    clearer & guiding constructors

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,43 +1,35 @@
 # -*- coding: utf-8 -*-
 
-def _mod_quality(item, inc=1):
-    q = item.quality + inc
+from functools import partial
+
+def _modify_quality(item, by=1):
+    q = item.quality + by
     if q > 50: q = 50
     if q <0: q = 0
     return q
 
-def _aged_normal(item):
-    return Item(item.name, item.sell_in - 1, _mod_quality(item, -1))
-
-def _aged_conjured(item):
-    return Item(item.name, item.sell_in - 1, _mod_quality(item, -2))
-
-def _aged_brie(item):
-    return Item(item.name, item.sell_in - 1, _mod_quality(item, 1))
-
-def _aged_backstage(item):
-    def aged_quality_backstage(item):
-        if item.sell_in > 10:
-            return _mod_quality(item, 1)
-        elif item.sell_in > 5:
-            return _mod_quality(item, 2)
-        elif item.sell_in > 0:
-            return _mod_quality(item, 3)
-        else:
-            return _mod_quality(item, -item.quality)
-    return Item(item.name, item.sell_in - 1, aged_quality_backstage(item))
-
-def _aged_sulfuras(item):
-    return Item(item.name, item.sell_in, item.quality)
-
 def _aged(item):
-        item_ager = {
-            Item.NAMES.AGED_BRIE:        _aged_brie,
-            Item.NAMES.CONJURED:         _aged_conjured,
-            Item.NAMES.BACKSTAGE_PASSES: _aged_backstage,
-            Item.NAMES.SULFURAS:         _aged_sulfuras,
-        }.get(item.name, _aged_normal)
-        return item_ager(item)
+    if item.name == Item.NAMES.AGED_BRIE:
+        return Item.create_brie(item.sell_in - 1, _modify_quality(item, by=1))
+
+    if item.name == Item.NAMES.CONJURED:
+        return Item.create_conjured(item.sell_in - 1, _modify_quality(item, by=-2))
+
+    if item.name == Item.NAMES.BACKSTAGE_PASSES:
+        def _aged_quality_backstage(item):
+            if item.sell_in > 10:
+                return _modify_quality(item, by=1)
+            if item.sell_in > 5:
+                return _modify_quality(item, by=2)
+            if item.sell_in > 0:
+                return _modify_quality(item, by=3)
+            return _modify_quality(item, by=-item.quality)
+        return Item.create_backstage(item.sell_in - 1, _aged_quality_backstage(item))
+
+    if item.name == Item.NAMES.SULFURAS:
+        return Item.create_sulfuras(item.sell_in, item.quality)
+
+    return Item.create_normal(item.name, item.sell_in - 1, _modify_quality(item, by=-1))
 
 def update_quality(items):
     return [_aged(item) for item in items]
@@ -49,6 +41,26 @@ class Item:
         CONJURED = "Conjured Mana Cake"
         BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
         SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+    @classmethod
+    def create_normal(cls, name, sell_in, quality):
+        return cls(name, sell_in, quality)
+
+    @classmethod
+    def create_conjured(cls, sell_in, quality):
+        return cls(cls.NAMES.CONJURED, sell_in, quality)
+
+    @classmethod
+    def create_brie(cls, sell_in, quality):
+        return cls(cls.NAMES.AGED_BRIE, sell_in, quality)
+
+    @classmethod
+    def create_sulfuras(cls, sell_in, quality):
+        return cls(cls.NAMES.SULFURAS, sell_in, quality)
+
+    @classmethod
+    def create_backstage(cls, sell_in, quality):
+        return cls(cls.NAMES.BACKSTAGE_PASSES, sell_in, quality)
 
     def __init__(self, name, sell_in, quality):
         self.name = name
